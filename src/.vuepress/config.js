a80259308104bafc1d38148ba0d50a34077c18f6
singcl <1276837689@qq.com>
2022-09-21 10:46:31 +0800

    feat(vuepress): config

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,13 +1,19 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+console.log('directory-name 👉️', __dirname);
+
 // .vuepress/config.js
 export default {
-  themeConfig: {
-    sidebar: "auto",
-    nav: [
-      { text: '回首页', link: '/' },
-    ]
-  },
-  alias: {
-    'img': path.resolve(__dirname, './../img'),
-  },
+    themeConfig: {
+        sidebar: 'auto',
+        nav: [{ text: '回首页', link: '/' }]
+    },
+    alias: {
+        img: path.resolve(__dirname, './../img')
+    }
 };
